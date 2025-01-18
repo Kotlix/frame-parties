dec5a3f43a3a796d73b06b1b9d565c0eb037aeca
Enzhine <onarushkashamaev@gmail.com>
2025-01-18 19:06:05 +0300

    Gitlab init

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    kotlin("jvm") apply false
+    kotlin("plugin.spring") apply false
+    id("org.springframework.boot") apply false
+    id("org.jlleitschuh.gradle.ktlint") apply false
+    id("io.spring.dependency-management")
+}
+
+subprojects {
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+        plugin("org.jlleitschuh.gradle.ktlint")
+    }
+
+    val springBootVersion: String by project
+    val springCloudVersion: String by project
+
+    val groupId: String by project
+    val versionIdNumber: String by project
+    val versionIdStatus: String by project
+
+    group = groupId
+    val versionId: String = if (versionIdStatus.isEmpty()) versionIdNumber else "$versionIdNumber-$versionIdStatus"
+    version = versionId
+
+    dependencyManagement {
+        imports {
+            mavenBom("org.springframework.boot:spring-boot-dependencies:$springBootVersion")
+            mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+        }
+    }
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+}
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+        freeCompilerArgs.add("-Xjsr305=strict")
+    }
+}
---
+++ frame-parties-server/build.gradle.kts
@@ -0,0 +1,17 @@
+dependencies {
+    api(project(":frame-parties-api"))
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    implementation("org.springframework:spring-context-support")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.liquibase:liquibase-core")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+
+    runtimeOnly("org.postgresql:postgresql")
+}
